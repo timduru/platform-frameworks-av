0ef0c3215e45c3c96dd8fa4550361cce899fe7d6
Praveen Chavan <pchavan@codeaurora.org>
2014-02-06 18:58:35 -0800

    stagefright: Enable GraphicBuffer usage to decoder before querying formats
    This lets the decoder publish appropriate color format based on surface
    OR non-surface mode.
    
    Change-Id: I567ae53d4d0dfa2abbf29e8196637a1da3e89969
    
    Conflicts:
    	media/libstagefright/OMXCodec.cpp

--- media/libstagefright/OMXCodec.cpp
+++ media/libstagefright/OMXCodec.cpp
@@ -779,6 +779,19 @@ status_t OMXCodec::configureCodec(const sp<MetaData> &meta) {
                 return err;
             }
         } else {
+
+            if (mNativeWindow != NULL
+                && !strncmp(mComponentName, "OMX.", 4)) {
+                status_t err = initNativeWindow();
+                if (err != OK) {
+                    return err;
+                }
+            }
+
+#ifdef QCOM_HARDWARE
+            ExtendedCodec::configureVideoDecoder(
+                    meta, mMIME, mOMX, mFlags, mNode, mComponentName);
+#endif
             status_t err = setVideoOutputFormat(
                     mMIME, meta);
 
@@ -831,16 +844,6 @@ status_t OMXCodec::configureCodec(const sp<MetaData> &meta) {
         }
 
         mQuirks &= ~kOutputBuffersAreUnreadable;
-    }
-
-    if (mNativeWindow != NULL
-        && !mIsEncoder
-        && !strncasecmp(mMIME, "video/", 6)
-        && !strncmp(mComponentName, "OMX.", 4)) {
-        status_t err = initNativeWindow();
-        if (err != OK) {
-            return err;
-        }
     }
 
     return OK;
