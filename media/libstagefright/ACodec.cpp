CyanogenMod/android_frameworks_av

Allow to use baseline profile for AVC recording
- some encoder seem to crash using higher h264 profiles

Change-Id: I2beb881e76519f872e3e99957f8b981eeaa53b56

media/libstagefright/ACodec.cpp
@@ -3568,7 +3568,7 @@ status_t ACodec::setupAVCEncoderParameters(const sp<AMessage> &msg) {
 
     // XXX
     // Allow higher profiles to be set since the encoder seems to support
-#if 0
+#ifndef USE_AVC_BASELINE_PROFILE
     if (h264type.eProfile != OMX_VIDEO_AVCProfileBaseline) {
         ALOGW("Use baseline profile instead of %d for AVC recording",
             h264type.eProfile);

timduru/platform-frameworks-av

media/libstagefright/ACodec.cpp
@@ -4384,7 +4384,7 @@ status_t ACodec::setupAVCEncoderParameters(const sp<AMessage> &msg) {
         h264type.eProfile = static_cast<OMX_VIDEO_AVCPROFILETYPE>(profile);
         h264type.eLevel = static_cast<OMX_VIDEO_AVCLEVELTYPE>(level);
     } 
-#if 0
+#ifdef USE_AVC_BASELINE_PROFILE
       else {
         h264type.eProfile = OMX_VIDEO_AVCProfileBaseline;
 #if 0   /* DON'T YET DEFAULT TO HIGHEST PROFILE */